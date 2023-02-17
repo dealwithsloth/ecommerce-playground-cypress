f3073b33e867b0f9bbd221f79d8888a04db5a2e2
dealwithsloth <kukinh@gmail.com>
2023-02-18 00:19:40 +0100

    cypress upgrade

--- cypress/fixtures/productListURLs.js
+++ cypress/fixtures/productListURLs.js
@@ -3,7 +3,6 @@ export default [
     "/index.php?route=product/product&product_id=31",
     "/index.php?route=product/product&product_id=107",
     "/index.php?route=product/product&path=17&product_id=28",
-    "/index.php?route=product/product&path=17&product_id=32",
     "/index.php?route=product/product&path=17&product_id=33",
     "/index.php?route=product/product&path=17&product_id=34",
     "/index.php?route=product/product&path=17&product_id=40",
