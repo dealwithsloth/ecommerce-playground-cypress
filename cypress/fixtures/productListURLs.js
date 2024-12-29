a81cd0a8658cec9adf9225229dd3d84cc8db09e8
dealwithsloth <49123381+dealwithsloth@users.noreply.github.com>
2024-12-29 07:55:12 +0100

    cypress upgrade and minor changes

--- cypress/fixtures/productListURLs.js
+++ cypress/fixtures/productListURLs.js
@@ -1,6 +1,3 @@
 export default [
-    "/index.php?route=product/product&product_id=31",
     "/index.php?route=product/product&path=17&product_id=28",
-    "/index.php?route=product/product&path=17&product_id=33",
-    "/index.php?route=product/product&path=17&product_id=34",
 ]
